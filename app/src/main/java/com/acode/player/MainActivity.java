c92ce07e4e7efd622954493f2970eb31fdff809a
yangtao <workertao@163.com>
2018-05-22 10:04:47 +0800

    update

--- app/src/main/java/com/acode/player/MainActivity.java
+++ app/src/main/java/com/acode/player/MainActivity.java
@@ -4,21 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.multidex.MultiDex;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.acode.player.lib.AcodePlayerView;
 import com.acode.player.lib.bean.PlayerBean;
 import com.acode.player.lib.data.Data;
 import com.acode.player.lib.tablayout.TestActivity;
-import com.acode.player.lib.utils.NetUtils;
 import com.acode.player.lib.utils.PermissionUtils;
 
 public class MainActivity extends AcodeBaseActivity {
